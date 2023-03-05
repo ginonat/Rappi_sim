e1bb29ff6ec3e9119355957581c73d010b1dbc50
Ginonat <Ginonat07@gmail.com>
2023-03-05 02:05:16 +0100

    added a runner struct that stores the current node the runner is tanding in, and can now teleport to neighboring nodes using left/right keyreleas

--- main.cpp
+++ main.cpp
@@ -7,23 +7,11 @@ int main()
     sf::RenderWindow window(sf::VideoMode(640, 480), "My Window", sf::Style::Titlebar | sf::Style::Close);
     window.clear(sf::Color::Black);
 
-    // Create a green box used for sanity checks
-    sf::RectangleShape SanityCheck(sf::Vector2f(10,10));
-    SanityCheck.setPosition(0, 0);
-    SanityCheck.setFillColor(sf::Color::Green);
-
-    // Create a red runner
-    sf::RectangleShape runner(sf::Vector2f(10,10));
-    runner.setPosition(640, 480);
-    runner.setFillColor(sf::Color::Red);
-    // previous runner position
-    sf::Vector2f prevRunnerPos;
-
-
     struct Node {
         sf::Vector2f position;
         std::vector<Node*> neighbors;
     };
+
 
     // Create nodes
     const int rows = 10;
@@ -64,26 +52,56 @@ int main()
         nodes_sprit.push_back(circle);
     }
 
-    // Create a vertex array for the lines
-    sf::VertexArray lines(sf::Lines, 0);
+    // Create a vertex array for the streets
+    sf::VertexArray streets(sf::Lines, 0);
     
-    // Add the lines to the vertex array
+    // Add the streets to the vertex array
     for (const auto& node : nodes) {
         for (const auto& neighbor : node.neighbors) {
             sf::Vertex vertex1(node.position, sf::Color::White);
             sf::Vertex vertex2(neighbor->position, sf::Color::White);
-            lines.append(vertex1);
-            lines.append(vertex2);
+            streets.append(vertex1);
+            streets.append(vertex2);
         }
     }
     
-    // Set the outline thickness of the lines
-    lines.setPrimitiveType(sf::Lines);
+    // Set the outline thickness of the streets
+    streets.setPrimitiveType(sf::Lines);
 
 
 
+
+
+
+    // runners struct
+    struct runner {
+        sf::RectangleShape box;
+        Node* current_node;
+    };
+
+    // Create a red runner
+    runner runner_A;
+    // starting initial node
+    runner_A.current_node=&nodes[35];
+    // set sprit box size 
+    runner_A.box.setSize(sf::Vector2f(10,10));
+    // paint runner_A red
+    runner_A.box.setFillColor(sf::Color::Red);
+    // place runner_A to node position
+    runner_A.box.setPosition(runner_A.current_node->position);
     // Set the movement speed to 0.01 pixels per frame
     float movementSpeed = 0.01f;
+
+// sanity check
+// Print out the current node and its neighbors for each runner
+//    std::cout << "Runner position: " << runner_A.position.x << ", " << runner_A.position.y << std::endl;
+    std::cout << "Current node position: " << runner_A.current_node->position.x << ", " << runner_A.current_node->position.y << std::endl;
+    std::cout << "Neighbor nodes positions: ";
+    for (const auto& n : runner_A.current_node->neighbors) {
+        std::cout << "(" << n->position.x << ", " << n->position.y << ") ";
+    }
+    std::cout << std::endl;
+
 
     // Run the game loop
     while (window.isOpen()) {
@@ -93,6 +111,27 @@ int main()
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
+
+           // Check if the event is a key release
+            if (event.type == sf::Event::KeyReleased) {
+                // Check if the key released is the left arrow key
+                if (event.key.code == sf::Keyboard::Right) {
+                    // Change the current node of runner_A to one of its neighbors
+                    if (!runner_A.current_node->neighbors.empty()) {
+                        runner_A.box.setPosition(runner_A.current_node->neighbors[1]->position);
+                        std::cout << "Runner A moved to node at position (" << runner_A.current_node->neighbors[1]->position.x << ", " << runner_A.current_node->neighbors[1]->position.y << ")" << std::endl;
+                        runner_A.current_node=runner_A.current_node->neighbors[1];
+                    }
+                }
+                if (event.key.code == sf::Keyboard::Left) {
+                    // Change the current node of runner_A to one of its neighbors
+                    if (!runner_A.current_node->neighbors.empty()) {
+                        runner_A.box.setPosition(runner_A.current_node->neighbors[0]->position);
+                        std::cout << "Runner A moved to node at position (" << runner_A.current_node->neighbors[0]->position.x << ", " << runner_A.current_node->neighbors[0]->position.y << ")" << std::endl;
+                        runner_A.current_node=runner_A.current_node->neighbors[0];
+                    }
+                }
+            }
         }
 
         // Check if the shift key is pressed
@@ -101,53 +140,41 @@ int main()
             // Increase the movement speed if the shift key is pressed
             movementSpeed = 0.03f;
         }
-    else
-    {
+        else
+        {
             // Reset the movement speed if the shift key is not pressed
             movementSpeed = 0.01f;
         }
         // Update the runner's position based on the arrow keys
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
         {
-            runner.move(-movementSpeed, 0);
+            runner_A.box.move(-movementSpeed, 0);
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
         {
-            runner.move(movementSpeed, 0);
+            runner_A.box.move(movementSpeed, 0);
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
         {
-            runner.move(0, -movementSpeed);
+            runner_A.box.move(0, -movementSpeed);
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
         {
-            runner.move(0, movementSpeed);
+            runner_A.box.move(0, movementSpeed);
         }
 
         // Stop the runner from leaving the window
 
-        if (runner.getPosition().x < 0)
-            runner.setPosition(0, runner.getPosition().y);
-        if (runner.getPosition().y < 0)
-            runner.setPosition(runner.getPosition().x, 0);
-        if (runner.getPosition().x + runner.getSize().x > window.getSize().x)
-            runner.setPosition(window.getSize().x - runner.getSize().x, runner.getPosition().y);
-        if (runner.getPosition().y + runner.getSize().y > window.getSize().y)
-            runner.setPosition(runner.getPosition().x, window.getSize().y - runner.getSize().y);
-
-
-//        // keep runer in the streets
-//        in_street=false;
-//        for(auto& street : streets){
-//            if ((runner.getGlobalBounds().intersects(street.getGlobalBounds()))){
-//                in_street=true;
-//            }
-//        }
-//        if (!(in_street)){
-//            runner.setPosition(prevRunnerPos);
-//        }
+//        if (runner.getPosition().x < 0)
+//            runner.setPosition(0, runner.getPosition().y);
+//        if (runner.getPosition().y < 0)
+//            runner.setPosition(runner.getPosition().x, 0);
+//        if (runner.getPosition().x + runner.getSize().x > window.getSize().x)
+//            runner.setPosition(window.getSize().x - runner.getSize().x, runner.getPosition().y);
+//        if (runner.getPosition().y + runner.getSize().y > window.getSize().y)
+//            runner.setPosition(runner.getPosition().x, window.getSize().y - runner.getSize().y);
 //
-//
+
         // Clear the window
         window.clear(sf::Color::Black);
 
@@ -156,12 +183,12 @@ int main()
         for (const auto& node : nodes_sprit) {
             window.draw(node);
         }
-        // Draw lines
-//        window.draw(lines.data(), lines.size(), sf::Lines);
-        window.draw(lines);
+        // Draw streets
+//        window.draw(streets.data(), streets.size(), sf::streets);
+        window.draw(streets);
 
         // Draw the runner
-        window.draw(runner);
+        window.draw(runner_A.box);
 
         // Display the window
         window.display();
